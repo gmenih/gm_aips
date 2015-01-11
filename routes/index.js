f829d39055a665e575c47400940d8a44f0ee58ee
gmenih341 <gmenih@gmail.com>
2015-01-11 15:41:07 +0100

    added placeholder routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.send('Pozdravljeni na AIPS spletni strani!');
+});
+
+/* GET login page */
+router.get('/login', function(req, res){
+  res.render('login', {title: 'Prijava'});
+});
+
+/* POST login */
+router.post('/login', function(req, res){
+  //TODO creaate session
+  res.send('working on it ;)');
+});
+/* GET logout */
+router.get('/logout', function(req, res){
+  // TODO delete session
+  res.send('working on it ;)');
 });
 
 module.exports = router;
